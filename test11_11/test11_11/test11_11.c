9df20e3a5260cc88efe4cd2c914c1cb6bd6ea85a
Elysia <104663915+3189489067@users.noreply.github.com>
2025-11-11 23:11:07 +0800

    Add files via upload

---
+++ test11_11/test11_11/test11_11.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+int i = 0;
+int ret = 1;
+int sum = 0;
+int n = 0;
+
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        ret *= i;
+        sum += ret;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
